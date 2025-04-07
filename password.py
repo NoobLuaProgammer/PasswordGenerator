bb73430fd7527fbc93533f0905a854d75d531013
NoobLuaProgammer <82332559+NoobLuaProgammer@users.noreply.github.com>
2025-04-07 17:45:30 +0100

    Update password.py

--- password.py
+++ password.py
@@ -1,3 +1,5 @@
 import random
-characters = ["a", "b", "c"]
-length = input("How long do you want the password to be?: ")
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '=', '_', '-', '!', '\\', '/', '?', '<', '>', '.', ',', '@', "'", ';', ':', '#', '~', '[', ']', '{', '}', '"', '£', '$', '%', '^', '&', '*', '(', ')']
+length = int(input("How long do you want the password to be?: "))
+result = ''.join(random.choice(characters) for i in range(length))
+print(result)
